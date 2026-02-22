9432a938e4bd07d5ee8dc5fea8aaa408f0368c98
Hugo Bouverat <hugo.b74@protonmail.ch>
2026-02-22 19:45:24 +0100

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake for my Homelab Repo.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+	  inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go-task
+          ];
+
+          shellHook = ''
+            echo "Homelab Flake Environment loaded."
+          '';
+        };
+      }
+    );
+}
